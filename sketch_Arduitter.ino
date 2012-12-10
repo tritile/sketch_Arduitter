c864db213ecb295b047b4e09213a7d7d423804bd
Bruno Martins <tritile@yahoo.com.br>
2012-12-10 00:33:33 -0200

    Receive and print the message

--- sketch_Arduitter.ino
+++ sketch_Arduitter.ino
@@ -1,23 +1,66 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#define numLines 4
+#define numRows 20
+#define NL 10
+
+char message[80]; // Maximum number of caracters supported by lcd display
 
 void setup(){
   
- lcd.begin(20, 4);
+ lcd.begin(numRows, numLines);
   
  Serial.begin(9600); 
   
 }
 
-void loop(){
+int receiveMessage(){
+  
+  boolean flagEndOfMenssage; // End message flag
+  int i;
+  
+  flagEndOfMenssage=0;
+  i=0;
+  
+  while (!flagEndOfMenssage) {
+    if (Serial.available()) {
+      message[i] = Serial.read();
+      if (message[i]==NL) {
+        message[i+1] = 0;
+        flagEndOfMenssage = 1;
+      }
+      i++;      
+    }
+  }
+  
+  return (i);
+  
+}
+
+void printMessage(int numberOfCaracters){
+  
+//  char singleLineTemp
   
   lcd.clear();
   
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0,0);
   
-  lcd.print(F("Teste ArWSGDFHFGHVFDduitter"));
+  lcd.print(message);
   
-  while(1);
+  lcd.setCursor(0,1);
+  
+  lcd.print(numberOfCaracters);
+  
+  lcd.print(" caracteres");
+  
+  return;  
+  
+}
+
+void loop(){
+  
+  printMessage (receiveMessage ());  
   
 }
